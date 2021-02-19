5b0a9036fd7bcaf5601296642ba8571909cf779c
AmrElsersy <amrelsersay@gmail.com>
2021-02-19 04:20:07 +0200

    euler angles demo camera
    Signed-off-by: AmrElsersy <amrelsersay@gmail.com>

--- camera_demo.py
+++ camera_demo.py
@@ -3,6 +3,7 @@ import time
 import argparse
 import cv2
 import numpy as np
+from numpy.lib.type_check import imag
 import torch
 import torchvision.transforms.transforms as transforms
 from face_detector.face_detector import DnnDetector, HaarCascadeDetector
@@ -11,6 +12,28 @@ from euler_angles import EulerAngles
 
 sys.path.insert(1, 'face_detector')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def draw_euler_angles(frame, face, axis_pts, euler_angles):
+    (x,y,w,h) = face
+    top_left = (x,y)
+    center = (x+w//2, y+h//2)
+
+    axis_pts = axis_pts.astype(np.int32)
+    pitch_point = tuple(axis_pts[0].ravel() + top_left)
+    yaw_point   = tuple(axis_pts[1].ravel() + top_left)
+    roll_point  = tuple(axis_pts[2].ravel() + top_left)
+
+    width = 3
+    cv2.line(frame, center,  pitch_point, (0,255,0), width)
+    cv2.line(frame, center,  yaw_point, (255,0,0), width)
+    cv2.line(frame, center,  roll_point, (0,0,255), width)
+
+    pitch, yaw, roll = euler_angles
+    cv2.putText(frame, "Pitch:{:.2f}".format(pitch), (x,y+10), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,0))
+    cv2.putText(frame, "Yaw:{:.2f}".format(yaw), (x,y+25), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,0))
+    cv2.putText(frame, "Roll:{:.2f}".format(roll), (x,y+40), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,0))
+
+    return frame
 
 def main(args):
     # Models
@@ -45,6 +68,12 @@ def main(args):
 
         for face in faces:
             (x,y,w,h) = face
+            side = max(w,h)
+            d_side = int(0.1 * side)
+            x -= d_side//2
+            y -= d_side//2
+            w += d_side
+            h += d_side
 
             cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 3)
 
@@ -66,7 +95,13 @@ def main(args):
                 for (x_l, y_l) in landmarks:
                     cv2.circle(frame, (x + x_l, y + y_l), 2, (0,255,0), -1)
 
-        cv2.putText(frame, str(fps), (10,80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0))
+                if args.head_pose:
+                    rvec, tvec, euler_angles = head_pose.eular_angles_from_landmarks(np.copy(landmarks).astype(np.float))
+                    axis = np.identity(3) * 7
+                    axis_pts = cv2.projectPoints(axis, rvec, tvec, head_pose.camera_intrensic_matrix, None)[0]
+                    frame = draw_euler_angles(frame, (x,y,w,h), axis_pts, euler_angles)
+
+        cv2.putText(frame, str(fps), (10,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0))
         cv2.imshow("Video", frame)   
         # cv2.imshow("black", face_frame)
         if cv2.waitKey(1) & 0xff == 27:
@@ -78,6 +113,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--haar', action='store_true', help='run the haar cascade face detector')
     parser.add_argument('--pretrained',type=str,default='checkpoint/model_weights/weights.pth.tar',help='load weights')
+    parser.add_argument('--head_pose', action='store_true', help='visualization of head pose euler angles')
     args = parser.parse_args()
 
     main(args)
